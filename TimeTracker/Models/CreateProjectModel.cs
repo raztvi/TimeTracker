5bd48f8ce5a5f0e51fdc279755c8356441f670a4
razvan <raztvi@yahoo.com>
2017-07-18 18:17:44 +0300

    Tweaks to projects details

--- TimeTracker/Models/CreateProjectModel.cs
+++ TimeTracker/Models/CreateProjectModel.cs
@@ -9,11 +9,16 @@ namespace TimeTracker.Models
     public class CreateProjectModel
     {
         public int ID { get; set; }
+        [Required]
+        [Display(Name ="Project Name")]
         public string ProjectName { get; set; }
+        [Display(Name = "Task Name")]
         public string TaskName { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
     }
 }
