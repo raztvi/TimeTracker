d586126b868ec28ba867a46575ba8519b033ec3f
razvan <raztvi@yahoo.com>
2017-07-19 17:28:41 +0300

    Added datepicker

--- TimeTracker/wwwroot/js/site.js
+++ TimeTracker/wwwroot/js/site.js
@@ -1,5 +1,1 @@
 ﻿// Write your Javascript code.
-<link rel="stylesheet" type="text/css" href="/jquery.datetimepicker.css" />
-    <script src="/jquery.js"></script>
-    <script src="/build/jquery.datetimepicker.full.min.js"></script>
-<link rel="stylesheet" href="/build/jquery.timepicker.min.css"/>
